403890c1ba736143f42c01a0740dc8a8021b9e0d
yaoqtan <1148559020@qq.com>
2023-01-14 23:08:49 +0800

    整个项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <div class="shop-name">商品名称: {{shop.name}}</div>
+    <div class="shop-price">商品价格: {{shop.price}}</div>
+    <button @click="showShopInfo()">show</button>
+  </div>
+</template>
+<script>
+import { ref } from '@vue/reactivity'
+import { watchEffect } from '@vue/runtime-core'
+export default {
+  name: 'HelloWorld',
+  props: {
+    shop: Object
+  },
+  setup(props){
+    let shop = ref({})
+    shop.value = props.shop
+    const showShopInfo = ()=>{
+      console.log(props.shop)
+      // console.log(shop.value)
+    }
+
+    // watchEffect(()=>{
+    //   shop.value = props.shop
+    // })
+
+    return {
+      shop,
+      showShopInfo
+    }
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home">
+    <div class="shop-list">
+      <div class="item" v-for="(item,index) in shopList" :key="index" @click="showShowInfo(item)">
+        <div class="name">{{item.name}}</div>
+        <div class="price">{{item.price}}</div>
+      </div>
+    </div>
+
+    <HelloWorld :shop="showItem" v-if="isShowShowInfo"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive,ref } from '@vue/reactivity'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  setup(){
+    let isShowShowInfo = ref(false)
+    let shopList = reactive([
+      {
+        name:"小米11",
+        price:3000
+      },
+      {
+        name:"苹果11",
+        price:6000
+      },
+      {
+        name:"华为meta50",
+        price:10000
+      }
+    ])
+    let showItem = ref({})
+    const showShowInfo = (item)=>{
+      showItem.value = item
+      isShowShowInfo.value = true
+    }
+
+
+
+
+  return {
+    shopList,
+    showItem,
+    showShowInfo,
+    isShowShowInfo
+  }
+
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.shop-list{
+  width: 100%;
+  .item{
+    display: flex;
+    width: 150px;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    margin-bottom: 20px;
+  }
+}
+</style>
